25177104ce32e70118d57d0605002ee74a291910
ezevin <ehzevin@gmail.com>
2019-05-24 15:33:35 -0400

    some routes

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom'
+
+const initialState = {
+  error: false,
+  fields: {
+    username: '',
+    password: ''
+  }
+}
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState
+  }
+
+  handleChange = e => {
+    const newFields = { ...this.state.fields, [e.target.name]: e.target.value };
+    this.setState({ fields: newFields });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault()
+    // console.log(this.state.fields);
+    fetch('http://localhost:3001/api/v1/users', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify(this.state.fields)
+    })
+    .then(r => r.json())
+    .then(data => {
+      if (data.error) {
+        this.setState({error: true})
+      } else {
+        this.props.handleUserLogin(data)
+        this.props.history.push("/")
+        // console.log('data from api', data)
+      }
+    })
+
+    // this.setState(initialState)
+  };
+
+  render() {
+    console.log('Login props', this.props);
+    const { fields } = this.state
+    return (
+      <div>
+        <div className="ui form error">
+          {
+            this.state.error &&
+            <div className="ui error message">
+              Try Again
+            </div>
+          }
+          <form onSubmit={this.handleSubmit}>
+            <div className="ui field">
+              <label>Username</label>
+              <input
+                name="username"
+                placeholder="username"
+                value={fields.username}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="ui field">
+              <label>Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="password"
+                value={fields.password}
+                onChange={this.handleChange}
+              />
+            </div>
+            <button type="submit" className="ui basic green button">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Login);
---
+++ frontend/src/components/MyShelf.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { Button, Card, Image } from 'semantic-ui-react'
+
+class MyShelf extends Component {
+
+  state = {
+    details: false
+  }
+
+  toggleDetails = () => {
+    this.setState({details: !this.state.details})
+  }
+
+  render(){
+      const { title, author, genre, image, id } = this.props.books
+      if(this.state.details === false){
+      return(
+        <center>
+          <Card.Group itemsPerRow={2}>
+            <Card raised image={image}>
+              <div id={id} className="card two wide column">
+                  <Image class="ui image" src={image} alt={title} onClick={this.toggleDetails} />
+              </div>
+            </Card>
+          </Card.Group>
+        </center>
+      )}
+      else if (this.state.details === true){
+        return(
+          <center>
+            <Card>
+             <Card.Content>
+               <Image floated='right' size='mini' src={image} />
+               <Card.Header>{title}</Card.Header>
+               <Card.Meta>{author}</Card.Meta>
+               <Card.Description>
+                 {genre}
+               </Card.Description>
+             </Card.Content>
+             <Card.Content extra>
+               <div className='ui two buttons'>
+                 <Button basic color='green' onClick={() =>this.props.onClick(this.props.books)} positive>
+                   Add To BookShelf
+                 </Button>
+                 <Button.Or />
+                 <Button basic color='red' onClick={this.toggleDetails}>
+                   Cancel
+                 </Button>
+               </div>
+             </Card.Content>
+           </Card>
+          </center>
+        )
+      }
+  }
+}
+
+export default MyShelf
